f991d4e81e04531b25922c487e93163002e1f098
kainoa <lkainoa1121@foxmail.com>
2018-09-29 15:28:39 +0800

    修改bug

--- leanshi/leanshi/leanshi_member/src/main/java/cn/leanshi/StarMemberApplication.java
+++ leanshi/leanshi/leanshi_member/src/main/java/cn/leanshi/StarMemberApplication.java
@@ -2,22 +2,13 @@ package cn.leanshi;
 
 import cn.leanshi.model.util.DateConverter;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 /**
  * @author :ldq
@@ -36,20 +27,5 @@ public class StarMemberApplication {
 		return new DateConverter();
 	}
 
-	@Bean
-	public HttpMessageConverters fastJsonHttpMessageConverters() {
-		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-		FastJsonConfig fastJsonConfig = new FastJsonConfig();
-		//处理中文乱码问题
-		List<MediaType> fastMediaTypes = new ArrayList<>();
-		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-		fastConverter.setSupportedMediaTypes(fastMediaTypes);
-		fastConverter.setFastJsonConfig(fastJsonConfig);
-		fastJsonConfig.setDateFormat("yyyy-MM-dd");
-		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
-				SerializerFeature.DisableCircularReferenceDetect,SerializerFeature.WriteMapNullValue);
-		fastConverter.setFastJsonConfig(fastJsonConfig);
-		HttpMessageConverter<?> converter = fastConverter;
-		return new HttpMessageConverters(converter);
-	}
+	
 }
